efba9a6fbfdb0dfcfe7d279e637c50ccff826c66
Roman Niukhalov <r.nyukhalov@gmail.com>
2020-02-04 18:49:39 +0800

    Add lateral acceleration to the cost function for smoother turns

--- src/mpc/FG_eval.cpp
+++ src/mpc/FG_eval.cpp
@@ -13,13 +13,15 @@ using CppAD::Value;
 // ng = fg.size() - 1
 // fg[0] = f(x)
 // fg[1 + i] = g_i(x)
-void FG_eval::operator()(ADvector& fg, const ADvector& vars) {
+void FG_eval::operator()(ADvector &fg, const ADvector &vars)
+{
   // The cost is stored is the first element of `fg`.
   // Any additions to the cost should be added to `fg[0]`.
   fg[0] = 0;
 
   // Reference State Cost
-  for (size_t t = 0; t < config.num_states; ++t) {
+  for (size_t t = 0; t < config.num_states; ++t)
+  {
     AD<double> x = vars[t + config.x_start];
     AD<double> y = vars[t + config.y_start];
     AD<double> y_des = polyeval(coeffs, x);
@@ -29,12 +31,23 @@ void FG_eval::operator()(ADvector& fg, const ADvector& vars) {
     AD<double> epsi = psi - psi_des;
     AD<double> vel_err = (config.target_vel - vars[t + config.v_start]) / config.target_vel;
 
-    fg[0] += 10 * CppAD::pow(cte, 2);
+    fg[0] += CppAD::pow(cte, 2);
     fg[0] += 500 * CppAD::pow(epsi, 2);
     fg[0] += 300 * CppAD::pow(vel_err, 2);
+
+    if (t > 1)
+    {
+      AD<double> y_prev = vars[t - 1 + config.y_start];
+      AD<double> y_prev_prev = vars[t - 2 + config.y_start];
+      AD<double> y_vel = y - y_prev;
+      AD<double> y_vel_prev = y_prev - y_prev_prev;
+      AD<double> y_accel = y_vel - y_vel_prev;
+      fg[0] += 100 * CppAD::pow(y_accel, 2);
+    }
   }
 
-  for (size_t t = 0; t < config.num_actuations; ++t) {
+  for (size_t t = 0; t < config.num_actuations; ++t)
+  {
     AD<double> delta = vars[t + config.delta_start];
     AD<double> accel = vars[t + config.a_start];
 
@@ -42,7 +55,8 @@ void FG_eval::operator()(ADvector& fg, const ADvector& vars) {
     fg[0] += CppAD::pow(accel, 2);
   }
 
-  for (size_t t = 1; t < config.num_actuations; ++t) {
+  for (size_t t = 1; t < config.num_actuations; ++t)
+  {
     AD<double> accel_cur = vars[t + config.a_start];
     AD<double> accel_prev = vars[t - 1 + config.a_start];
     AD<double> delta_cur = vars[t + config.delta_start];
@@ -66,23 +80,24 @@ void FG_eval::operator()(ADvector& fg, const ADvector& vars) {
   fg[1 + config.v_start] = vars[config.v_start];
 
   // The rest of the constraints
-  for (size_t t = 1; t < config.num_states; ++t) {
-    AD<double> x1     = vars[t + config.x_start];
-    AD<double> y1     = vars[t + config.y_start];
-    AD<double> psi1   = vars[t + config.psi_start];
-    AD<double> v1     = vars[t + config.v_start];
+  for (size_t t = 1; t < config.num_states; ++t)
+  {
+    AD<double> x1 = vars[t + config.x_start];
+    AD<double> y1 = vars[t + config.y_start];
+    AD<double> psi1 = vars[t + config.psi_start];
+    AD<double> v1 = vars[t + config.v_start];
 
-    AD<double> x0         = vars[t - 1 + config.x_start];
-    AD<double> y0         = vars[t - 1 + config.y_start];
-    AD<double> psi0       = vars[t - 1 + config.psi_start];
-    AD<double> v0         = vars[t - 1 + config.v_start];
-    AD<double> delta0     = vars[t - 1 + config.delta_start];
-    AD<double> a0         = vars[t - 1 + config.a_start];
-    AD<double> y0_desired   = polyeval(coeffs, x0);
+    AD<double> x0 = vars[t - 1 + config.x_start];
+    AD<double> y0 = vars[t - 1 + config.y_start];
+    AD<double> psi0 = vars[t - 1 + config.psi_start];
+    AD<double> v0 = vars[t - 1 + config.v_start];
+    AD<double> delta0 = vars[t - 1 + config.delta_start];
+    AD<double> a0 = vars[t - 1 + config.a_start];
+    AD<double> y0_desired = polyeval(coeffs, x0);
 
     fg[1 + t + config.x_start] = x1 - (x0 + v0 * CppAD::cos(psi0) * config.dt);
     fg[1 + t + config.y_start] = y1 - (y0 + v0 * CppAD::sin(psi0) * config.dt);
-    fg[1 + t + config.psi_start] = psi1 - (psi0 + CppAD::tan(delta0) * (v0/config.lf) * config.dt);
+    fg[1 + t + config.psi_start] = psi1 - (psi0 + CppAD::tan(delta0) * (v0 / config.lf) * config.dt);
     fg[1 + t + config.v_start] = v1 - (v0 + a0 * config.dt);
   }
 }
